86d5ba91dd6d48fc857745a9b408eb73c882c003
宇野礼於 <leouno12@gmail.com>
2019-11-11 22:30:11 +0900

    :tada: first commit

---
+++ src/ServantApi.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DataKinds     #-}
+{-# LANGUAGE TypeOperators #-}
+
+module ServantApi
+  ( BaseCrudApi
+  , GetEntitiesApi
+  , PostEntitiesApi
+  , UpdateEntitiesApi
+  , DeleteEntitiesApi
+  ) where
+
+import           Servant
+
+-- API for values of type 'a'
+-- indexed by values of type 'i'
+type BaseCrudApi a i
+   = Get '[ JSON] [(i, a)] -- list 'a's
+      :<|> ReqBody '[ JSON] a :> PostNoContent '[ JSON] NoContent -- add an 'a'
+      :<|> Capture "id" i :> (Get '[ JSON] a -- view an 'a' given its "identifier" of type 'i'
+                               :<|> ReqBody '[ JSON] a :> PutNoContent '[ JSON] NoContent -- update an 'a'
+                               :<|> DeleteNoContent '[ JSON] NoContent -- delete an 'a'
+                              )
+
+type GetEntitiesApi a i
+   = ReqBody '[ JSON] [i] :> Post '[ JSON] [(i, a)] -- list 'a's specified by 'i's
+
+type PostEntitiesApi a i = ReqBody '[ JSON] [a] :> Post '[ JSON] [i] -- add 'a's
+
+type UpdateEntitiesApi a i
+   = ReqBody '[ JSON] [(i, a)] :> PutNoContent '[ JSON] NoContent -- update 'a's
+
+type DeleteEntitiesApi a i
+   = ReqBody '[ JSON] [i] :> DeleteNoContent '[ JSON] NoContent -- delete 'a's
